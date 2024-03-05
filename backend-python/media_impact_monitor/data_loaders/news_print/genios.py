ba73bef7b6a1d4cea63e01d8cc77306a3c32acd8
David Pomerenke <46022183+davidpomerenke@users.noreply.github.com>
2024-03-05 21:01:23 +0100

    feat(genios.py): add genios data loader

---
+++ backend-python/media_impact_monitor/data_loaders/news_print/genios.py
@@ -0,0 +1,31 @@
+from media_impact_monitor.util.cache import cloudcache, get
+import pandas as pd
+from beartype import beartype
+
+
+@cloudcache
+@beartype
+def get_counts(query: str, start_date: pd.Timestamp, end_date: pd.Timestamp):
+    response = get(
+        "https://www.genios.de/api/searchResult/Alle/Presse",
+        params={
+            "requestText": query,
+            "size": 0,
+            "sort": "BY_DATE",
+            "order": "desc",
+            "resultListType": "DEFAULT",
+            "date": [
+                f"from_{start_date.strftime('%d.%m.%Y')}",
+                f"to_{end_date.strftime('%d.%m.%Y')}",
+            ],
+            "getDateHistograms": "day",
+        },
+    )
+    data = response.json()["aggregations"]["day"]
+    data = [{"date": k, "count": v["count"]} for k, v in data.items()]
+    df = pd.DataFrame(data)
+    df["date"] = pd.to_datetime(df["date"], format="%d-%m-%Y")
+    df = df.set_index("date")
+    # there is a bug that sets the count at day -1 to 0
+    df = df[df.index >= start_date]
+    return df

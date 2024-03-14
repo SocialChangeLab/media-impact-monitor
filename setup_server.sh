bf106963b4d21e7abd0e0564680c9e2935ae9bd7
David Pomerenke <46022183+davidpomerenke@users.noreply.github.com>
2024-03-14 20:42:57 +0000

    ci(server): replace terraform with server setup script

---
+++ setup_server.sh
@@ -0,0 +1,35 @@
+# run this on a fresh Ubuntu 22.04 server to set it up for serving the Docker image
+
+# setup nginx to serve the app from port 80
+sudo apt update
+sudo apt install -y nginx certbot python3-certbot-nginx
+sudo systemctl start nginx
+NGINX_TEMPLATE=$(cat <<'EOF'
+server {
+    listen 80;
+    server_name api.mediaimpactmonitor.app;
+
+    location / {
+        proxy_pass http://localhost:8000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
+    }
+}
+EOF
+)
+echo "$NGINX_TEMPLATE" | sudo tee /etc/nginx/sites-available/default
+sudo nginx -t && sudo systemctl reload nginx
+
+# setup SSL with certbot
+# sudo certbot --nginx --non-interactive --agree-tos \
+#     -d api.mediaimpactmonitor.app \
+#     --email david@socialchangelab.org
+
+# install Docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+
+# pull the Docker image
+sudo docker pull socialchangelab/media-impact-monitor:latest

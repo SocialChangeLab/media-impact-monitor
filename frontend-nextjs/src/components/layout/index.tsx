4c781483bdba701dca2d796a7353d4f0604041ec
Lucas Vogel <contact@vogelino.com>
2024-03-15 13:56:14 -0300

    feat(frontend-nextjs): Add refine scaffold

---
+++ frontend-nextjs/src/components/layout/index.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { PropsWithChildren } from "react";
+import { Breadcrumb } from "../breadcrumb";
+import { Menu } from "../menu";
+
+export const Layout: React.FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <div className="layout">
+      <Menu />
+      <div className="content">
+        <Breadcrumb />
+        <div>{children}</div>
+      </div>
+    </div>
+  );
+};

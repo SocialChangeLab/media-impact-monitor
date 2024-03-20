ce7f5746b499cdded1f5c8e215af06b07c653fa7
David Pomerenke <46022183+davidpomerenke@users.noreply.github.com>
2024-03-20 11:21:59 +0000

    feat(api): define types for the API

--- backend-python/media_impact_monitor/api.py
+++ backend-python/media_impact_monitor/api.py
@@ -9,14 +9,16 @@ from functools import partial
 import pandas as pd
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
-from typing_extensions import Literal
 
-from media_impact_monitor.data_loaders.news_online.mediacloud_ import (
-    get_mediacloud_counts,
+from media_impact_monitor.types_ import (
+    Count,
+    Event,
+    EventSearch,
+    FulltextSearch,
+    Impact,
+    ImpactSearch,
+    TrendSearch,
 )
-from media_impact_monitor.data_loaders.news_print.genios import get_genios_counts
-from media_impact_monitor.data_loaders.protest.acled import get_acled_events
-from media_impact_monitor.types_ import CountJson, CountType, EventJson
 
 app = FastAPI()
 
@@ -31,43 +33,45 @@ parse_date = partial(pd.to_datetime, format="%Y-%m-%d")
 
 
 @app.get("/events/")
-def get_events(
-    types: list[Literal["protest"]],
-    keywords: list[str],
-    organizations: list[str],
-    start_date: str,
-    end_date: str,
-) -> list[EventJson]:
+def get_events(**search: EventSearch) -> tuple[EventSearch, list[Event]]:
     """Fetch events from the Media Impact Monitor database."""
-    events = get_acled_events(keyword=keywords[0])
-    start_date = parse_date(start_date)
-    end_date = parse_date(end_date)
-    # ... TODO
-    return events.to_dict(orient="records")[:50]
+    pass
+    # events = get_acled_events(keyword=query[0])
+    # start_date = parse_date(start_date)
+    # end_date = parse_date(end_date)
+    # # ... TODO
+    # return events.to_dict(orient="records")[:50]
 
 
-@app.get("/counts/")
-def get_counts(
-    types: list[CountType],
-    keywords: list[str],
-    start_date: str,
-    end_date: str,
-) -> dict[CountType, list[CountJson]]:
+@app.get("/trends/")
+def get_trends(**search: TrendSearch) -> tuple[TrendSearch, list[Count]]:
     """Fetch media item counts from the Media Impact Monitor database."""
-    start_date = parse_date(start_date)
-    end_date = parse_date(end_date)
-    results = {}
-    if "news_online" in types:
-        results["news_online"] = get_mediacloud_counts(
-            query=keywords[0],
-            start_date=start_date,
-            end_date=end_date,
-        ).to_dict(orient="records")[:50]
-    if "news_print" in types:
-        results["news_print"] = get_genios_counts(
-            query=keywords[0],
-            start_date=start_date,
-            end_date=end_date,
-        ).to_dict(orient="records")[:50]
-    # ... TODO
-    return results
+    pass
+    # start_date = parse_date(start_date)
+    # end_date = parse_date(end_date)
+    # results = {}
+    # if "news_online" in trend_type:
+    #     results["news_online"] = get_mediacloud_counts(
+    #         query=query[0],
+    #         start_date=start_date,
+    #         end_date=end_date,
+    #     ).to_dict(orient="records")[:50]
+    # if "news_print" in trend_type:
+    #     results["news_print"] = get_genios_counts(
+    #         query=query[0],
+    #         start_date=start_date,
+    #         end_date=end_date,
+    #     ).to_dict(orient="records")[:50]
+    # # ... TODO
+    # return results
+
+
+@app.get("/fulltexts/")
+def get_fulltexts(**search: FulltextSearch) -> tuple[FulltextSearch, list[Event]]:
+    """Fetch fulltexts from the Media Impact Monitor database."""
+    raise NotImplementedError
+
+
+@app.get("/impact/")
+def get_impact(**search: ImpactSearch) -> tuple[ImpactSearch, Impact]:
+    """Compute the impact of an event on a media trend."""
--- backend-python/media_impact_monitor/types_.py
+++ backend-python/media_impact_monitor/types_.py
@@ -1,33 +1,115 @@
 from dataclasses import dataclass
+from datetime import date
 from typing import Literal
 
-import pandas as pd
+#### General types ####
+
+Topic = Literal["climate_change"]
+Query = str  # for now, just a single keyword
+MediaSource = Literal["news_online", "news_print"]
+
+
+#### Event types ####
+
+EventType = Literal["protest"]
+EventSource = Literal["acled"]
+EventId = str
+
+
+@dataclass
+class EventSearch:
+    event_type: EventType
+    source: EventSource
+    start_date: date
+    end_date: date
+    topic: Topic | None = None
+    query: Query | None = None
+    organizations: list[str] | None = None
 
 
 @dataclass
 class Event:
-    date: pd.Timestamp
-    organization: str
+    id: EventId
+    event_type: EventType
+    source: EventSource
+    date: date
+    organizations: list[str]
+    topic: Topic
     description: str
+
+
+#### Trend types ####
+
+TrendType = Literal["keywords", "topics", "sentiments"]
 
 
 @dataclass
-class EventJson:
-    date: str
-    organization: str
-    description: str
+class TrendSearch:
+    trend_type: TrendType
+    media_source: MediaSource
+    start_date: date
+    end_date: date
+    topic: Topic | None = None
+    query: Query | None = None
 
 
 @dataclass
 class Count:
-    date: pd.Timestamp
+    date: date
     count: int
+
+
+#### Fulltext types ####
 
 
 @dataclass
-class CountJson:
-    date: str
-    count: int
+class FulltextSearch:
+    media_source: MediaSource
+    start_date: date
+    end_date: date
+    topic: Topic | None = None
+    query: Query | None = None
+    organizations: list[str] | None = None
 
 
-CountType = Literal["news_online", "news_print"]
+@dataclass
+class Fulltext:
+    date: date
+    title: str
+    text: str
+
+
+#### Impact types ####
+
+
+Cause = list[EventId]
+
+
+@dataclass
+class Effect:
+    trend_type: TrendType
+    media_source: MediaSource
+    topic: Topic | None = None
+    query: Query | None = None
+    # start_date, end_date can be derived from the EventIds
+
+
+method = Literal["synthetic_control", "interrupted_time_series"]
+
+
+@dataclass
+class ImpactSearch:
+    cause: Cause
+    effect: Effect
+    method: method
+
+
+@dataclass
+class Impact:
+    method_applicability: Literal["no", "maybe"]
+    method_applicability_reason: str | None
+    impact_average: dict[
+        int, float
+    ]  # impact estimate for each day around the average protest event
+    impact_average_upper: dict[int, float] | None
+    impact_average_lower: dict[int, float] | None

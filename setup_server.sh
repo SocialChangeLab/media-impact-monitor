9fbb8bef35492358190d7ab9ab07b04213a85cdf
David Pomerenke <46022183+davidpomerenke@users.noreply.github.com>
2024-03-25 19:11:06 +0000

    fix(setup_server.sh): remove upgrade lines

--- setup_server.sh
+++ setup_server.sh
@@ -13,7 +13,6 @@ server {
         proxy_pass http://localhost:8000;
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection 'upgrade';
         proxy_set_header Host \$host;
         proxy_cache_bypass \$http_upgrade;
     }
@@ -27,7 +26,6 @@ server {
         proxy_pass http://localhost:8001;
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection 'upgrade';
         proxy_set_header Host \$host;
         proxy_cache_bypass \$http_upgrade;
     }

b9ba4d2f7525de5a9a5f0980fcbfac6f84dbdd08
Lennart Klein <31488915+kleinlennart@users.noreply.github.com>
2024-04-04 12:23:40 +0200

    fix(climate_groups.py): add more orgs; fix JSO (UK) rename
    #27

--- backend-python/media_impact_monitor/data_loaders/protest/climate_groups.py
+++ backend-python/media_impact_monitor/data_loaders/protest/climate_groups.py
@@ -2,27 +2,32 @@
 # keys: as in ACLED (with exceptions, see below)
 # values: other names, e. g. in the original language
 movement_aliases = {
+    "Animal Rebellion": ["AR"],  # was renamed to "Animal Rising"
+    "Animal Rising": ["AR"],
     "Alternatiba": [],
     "Attac": [],
     "BUND": ["Bund für Umwelt und Naturschutz Deutschland"],
     "Declare Emergency": [],
     "DGUB: The Green Youth Movement": ["Den Grønne Ungdomsbevægelse"],
     "Ende Gelaende": [],
-    "Extinction Rebellion": [],
+    "Extinction Rebellion": ["XR"],
     "Fridays for Future": [],
     "Friends of the Earth": [],
     "Greenpeace": [],
     "Insulate Britain": [],
-    "Just Stop Oil (United Kingdom)": [],
+    "Just Stop Oil (UK)": [],
     "Just Stop Oil (Norway)": ["Stopp Oljeletinga"],
     "Last Renovation": ["Dernière Rénovation"],
     "Last Generation (Germany)": ["Letzte Generation"],
     "Last Generation (Austria)": ["Letzte Generation"],
     "Last Generation (Italy)": ["Ultima Generazione"],
+    "Last Generation (Czech Republic)": ["Poslední generace"],
     "Legambiente": [],
+    "NB: Emergency Break": ["Nødbremsen"], 
     "Renovate Switzerland": [],
     "Restore Passenger Rail": [],
     "Restore Wetlands Movement": ["Återställ Våtmarker"],
+    "DxE: Direct Action Everywhere": ["DxE"]
 }
 
 movements = list(movement_aliases.keys())
@@ -39,6 +44,7 @@ acled_keys = [
     .replace("Extinction Rebellion", "XR: Extinction Rebellion")
     .replace("Just Stop Oil (UK)", "Just Stop Oil")
     .replace("Just Stop Oil (Norway)", "Just Stop Oil")
+    .replace("Direct Action Everywhere", "DxE: Direct Action Everywhere")
     for k in movement_aliases.keys()
 ]
 

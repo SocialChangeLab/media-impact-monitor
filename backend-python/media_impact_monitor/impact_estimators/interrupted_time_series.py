16eb1bd8a18e478a3501bb653733c6d0e5575f85
David Pomerenke <46022183+davidpomerenke@users.noreply.github.com>
2024-04-04 17:51:16 +0000

    feat(interrupted_time_series.py): use sklearn models
    but does not work due to issue with lags

--- backend-python/media_impact_monitor/impact_estimators/interrupted_time_series.py
+++ backend-python/media_impact_monitor/impact_estimators/interrupted_time_series.py
@@ -2,10 +2,19 @@ from datetime import date
 
 import causalpy as cp
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import (  # noqa: F401
+    GradientBoostingRegressor,
+    RandomForestRegressor,
+)
+from sklearn.linear_model import (  # noqa: F401
+    ElasticNet,
+    Lasso,
+    LinearRegression,
+    Ridge,
+)
 
 
-def calculate_impact(event_date: date, df: pd.DataFrame, with_uncertainty: bool = True):
+def calculate_impact(event_date: date, df: pd.DataFrame):
     assert df.columns == ["count"]
     assert df.index.name == "date"
     assert isinstance(df.index, pd.DatetimeIndex)
@@ -15,25 +24,27 @@ def calculate_impact(event_date: date, df: pd.DataFrame, with_uncertainty: bool 
         df[f"count_lag_{i}"] = df["count"].shift(i)
     lag_params = [f"count_lag_{i}" for i in range(1, 8)]
     lag_params = " + ".join(lag_params)
+    # add weekday dummies
+    df["weekday"] = df.index.weekday
+    df = pd.get_dummies(df, columns=["weekday"], drop_first=True, dtype=int)
+    weekday_params = [f"weekday_{i}" for i in range(1, 7)]
+    weekday_params = " + ".join(weekday_params)
+    formula = f"count ~ 1 + {lag_params} + {weekday_params}"
     # crop the dataframe to 14 days before and after the event
     df = df.loc[
         event_date - pd.Timedelta(days=182) : event_date + pd.Timedelta(days=28)
     ]
-    formula = f"count ~ 1 + {lag_params}"
     df = df.dropna()
-    # fit the interrupted time series model
-    if with_uncertainty:
-        result = cp.pymc_experiments.InterruptedTimeSeries(
-            df,
-            pd.to_datetime(event_date),
-            formula=formula,
-            model=cp.pymc_models.LinearRegression(sample_kwargs={"random_seed": 0}),
-        )
-    else:
-        result = cp.skl_experiments.InterruptedTimeSeries(
-            df,
-            pd.to_datetime(event_date),
-            formula=formula,
-            model=LinearRegression(),
-        )
+    # check that all values are numeric
+    assert df.dtypes.apply(pd.api.types.is_numeric_dtype).all()
+    result = cp.skl_experiments.InterruptedTimeSeries(
+        df,
+        pd.to_datetime(event_date),
+        formula=formula,
+        # model=LinearRegression(),
+        # model=Ridge(),
+        # model=Lasso(),
+        model=RandomForestRegressor(),
+        # model=GradientBoostingRegressor(),
+    )
     return result
--- backend-python/media_impact_monitor/impact_estimators/interrupted_time_series_test.py
+++ backend-python/media_impact_monitor/impact_estimators/interrupted_time_series_test.py
@@ -1,6 +1,7 @@
 from datetime import date
 
 import matplotlib.pyplot as plt
+import numpy as np
 from media_impact_monitor.data_loaders.news_print.genios import get_genios_counts
 from media_impact_monitor.data_loaders.protest.acled import get_acled_events
 from media_impact_monitor.impact_estimators.interrupted_time_series import (
@@ -9,12 +10,25 @@ from media_impact_monitor.impact_estimators.interrupted_time_series import (
 from tqdm.auto import tqdm
 
 events = get_acled_events(
-    countries=["Germany"], start_date=date(2022, 1, 1), end_date=date(2022, 12, 31)
+    countries=["Germany"], start_date=date(2022, 3, 1), end_date=date(2022, 9, 30)
 )
 events = events[events["description"].str.contains("climate")]
+events = events.drop_duplicates(subset=["date"])
+events = events.iloc[:20]
+impacts = []
 for i, event in tqdm(events.iterrows(), total=events.shape[0]):
-    # date_ = date(2022, 9, 23)  # FFF climate strike
     df = get_genios_counts("Klimaschutz", date(2022, 1, 1), date(2022, 12, 31))
-    result = calculate_impact(event["date"], df, with_uncertainty=False)
-    fig, ax = result.plot()
-    plt.savefig(f"plots/event_{i}.png")
+    result = calculate_impact(event["date"], df)
+    impacts.append(result.post_impact)
+    # fig, ax = result.plot()
+    # plt.savefig(f"plots/event_{i}.png")
+impacts = np.array(impacts)
+# plot the mean and the 95% confidence interval
+plt.plot(impacts.mean(axis=0))
+plt.fill_between(
+    np.arange(impacts.shape[1]),
+    impacts.mean(axis=0) - 2 * impacts.std(axis=0),
+    impacts.mean(axis=0) + 2 * impacts.std(axis=0),
+    alpha=0.3,
+)
+plt.savefig("plots/impact.png")

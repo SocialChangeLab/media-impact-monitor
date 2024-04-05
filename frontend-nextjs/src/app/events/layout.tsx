f77a4a985b4d5e969234363cc53b45c5d6ebcb4c
Lucas Vogel <contact@vogelino.com>
2024-04-04 21:37:44 -0300

    ci(frontend): Attempt to repair deployment deps

--- frontend-nextjs/src/app/events/layout.tsx
+++ frontend-nextjs/src/app/events/layout.tsx
@@ -1,6 +1,6 @@
 import { Layout as BaseLayout } from '@components/layout'
-import React from 'react'
+import { PropsWithChildren } from 'react'
 
-export default async function Layout({ children }: React.PropsWithChildren) {
+export default async function Layout({ children }: PropsWithChildren<{}>) {
 	return <BaseLayout currentPage="blog_posts">{children}</BaseLayout>
 }
--- frontend-nextjs/src/providers/devtools/index.tsx
+++ frontend-nextjs/src/providers/devtools/index.tsx
@@ -1,16 +1,16 @@
-"use client";
+'use client'
 
 import {
-  DevtoolsPanel,
-  DevtoolsProvider as DevtoolsProviderBase,
-} from "@refinedev/devtools";
-import React from "react";
+	DevtoolsPanel,
+	DevtoolsProvider as DevtoolsProviderBase,
+} from '@refinedev/devtools'
+import { PropsWithChildren } from 'react'
 
-export const DevtoolsProvider = (props: React.PropsWithChildren) => {
-  return (
-    <DevtoolsProviderBase>
-      {props.children}
-      <DevtoolsPanel />
-    </DevtoolsProviderBase>
-  );
-};
+export const DevtoolsProvider = (props: PropsWithChildren<{}>) => {
+	return (
+		<DevtoolsProviderBase>
+			{props.children}
+			<DevtoolsPanel />
+		</DevtoolsProviderBase>
+	)
+}

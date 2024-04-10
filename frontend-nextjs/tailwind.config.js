c9585fd1d46cc22f05dc0890d17ed21771c2ee0f
Lucas Vogel <contact@vogelino.com>
2024-04-10 16:59:20 -0400

    feat(frontend): Add about page to the app

--- frontend-nextjs/tailwind.config.js
+++ frontend-nextjs/tailwind.config.js
@@ -49,6 +49,7 @@ module.exports = {
 	plugins: [
 		require('@tailwindcss/container-queries'),
 		require('@tailwindcss/forms'),
+		require('@tailwindcss/typography'),
 		require('tailwindcss-animate'),
 		plugin(function ({ addUtilities }) {
 			addUtilities({

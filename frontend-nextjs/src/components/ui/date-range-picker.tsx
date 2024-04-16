da9ced5f67dcb07733f4372f52592e0951ffbe54
Lucas Vogel <contact@vogelino.com>
2024-04-15 21:09:56 -0400

    fix(frontend): Fix recursion problem in date-range-picker

--- frontend-nextjs/src/components/ui/date-range-picker.tsx
+++ frontend-nextjs/src/components/ui/date-range-picker.tsx
@@ -12,7 +12,7 @@ import {
 } from '@/components/ui/popover'
 import { cn } from '@/utility/classNames'
 import { CalendarDays } from 'lucide-react'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export function DatePickerWithRange({
 	className,
@@ -24,6 +24,7 @@ export function DatePickerWithRange({
 	onChange?: (date: { from: Date; to: Date }) => void
 }) {
 	const [isOpen, setIsOpen] = useState(false)
+	const lastRange = useRef<DateRange | undefined>()
 	const [date, setDate] = useState<DateRange | undefined>(
 		defaultDateRange || {
 			from: new Date(2022, 0, 20),
@@ -33,6 +34,11 @@ export function DatePickerWithRange({
 
 	useEffect(() => {
 		if (!defaultDateRange?.from || !defaultDateRange?.to) return
+		const currFrom = lastRange.current?.from?.toISOString()
+		const currTo = lastRange.current?.to?.toISOString()
+		const unchangedFrom = defaultDateRange.from.toISOString() === currFrom
+		const unchangedTo = defaultDateRange.to.toISOString() === currTo
+		if (unchangedFrom && unchangedTo) return
 		setDate(defaultDateRange)
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [
@@ -44,7 +50,13 @@ export function DatePickerWithRange({
 
 	useEffect(() => {
 		if (!date?.from || !date?.to || isOpen) return
+		const currFrom = lastRange.current?.from
+		const currTo = lastRange.current?.to
+		const unchangedFrom = date.from.toISOString() === currFrom?.toISOString()
+		const unchangedTo = date.to.toISOString() === currTo?.toISOString()
+		if (unchangedFrom && unchangedTo) return
 		onChange({ from: date.from, to: date.to })
+		lastRange.current = date
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [isOpen, date?.from?.toISOString(), date?.to?.toISOString()])
 

fix(impact.py): fix computation of confidence interval

backend-python/media_impact_monitor/impact.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import scipy.stats
 
 from media_impact_monitor.events import get_events_by_id
 from media_impact_monitor.impact_estimators.interrupted_time_series import (
@@ -8,6 +9,12 @@ from media_impact_monitor.impact_estimators.interrupted_time_series import (
 from media_impact_monitor.trend import get_trend
 from media_impact_monitor.types_ import Impact, ImpactSearch, TrendSearch
 from media_impact_monitor.util.cache import cache
+
+
+def confidence_interval(data: list[float], confidence: float):
+    return scipy.stats.t.interval(
+        confidence, len(data) - 1, loc=np.mean(data), scale=scipy.stats.sem(data)
+    )
 
 
 @cache
@@ -37,8 +44,8 @@ def get_impact(q: ImpactSearch) -> Impact:
     impacts_df = pd.concat([df.reset_index(drop=True) for df in impacts], axis=1)
     impacts_df.index = impacts_df.index - hidden_days_before_protest
     average = impacts_df.mean(axis=1)
-    lower = impacts_df.mean(axis=1) - 2 * impacts_df.std(axis=1)
-    upper = impacts_df.mean(axis=1) + 2 * impacts_df.std(axis=1)
+    lower = impacts_df.apply(lambda x: confidence_interval(x, 0.95)[0], axis=1)
+    upper = impacts_df.apply(lambda x: confidence_interval(x, 0.95)[1], axis=1)
     impacts_dicts = [impact["count"].to_dict() for impact in impacts]
     return Impact(
         method_applicability="maybe",
@@ -48,3 +55,4 @@ def get_impact(q: ImpactSearch) -> Impact:
         impact_average_upper=upper.to_dict(),
         individual_impacts=dict(zip(events["event_id"], impacts_dicts)),
     )
+    # TODO: divide impact by number of events on that day (by the same org)
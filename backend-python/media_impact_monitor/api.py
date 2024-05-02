c62f025c56bbb39826843d353df57300750c627e
David Pomerenke <46022183+davidpomerenke@users.noreply.github.com>
2024-05-02 19:27:18 +0000

    ci(api.py): include git commit in api metadata

--- backend-python/media_impact_monitor/api.py
+++ backend-python/media_impact_monitor/api.py
@@ -5,6 +5,7 @@ Or, if necessary: `poetry run uvicorn media_impact_monitor.api:app --reload`
 """
 
 import logging
+import subprocess
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, HTTPException
@@ -30,9 +31,11 @@ from media_impact_monitor.types_ import (
 )
 from media_impact_monitor.util.date import verify_dates
 
+commit_hash = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode().strip()
+
 metadata = dict(
     title="Media Impact Monitor API",
-    version="0.1.2",
+    version=f"0.1.0+{commit_hash}",
     contact=dict(
         name="Social Change Lab",
         url="https://github.com/socialchangelab/media-impact-monitor",

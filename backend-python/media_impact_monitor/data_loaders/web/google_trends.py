e44646906da480070124630bfbb88d52eb599504
David Pomerenke <46022183+davidpomerenke@users.noreply.github.com>
2024-05-09 12:15:04 +0000

    fix(google_trends.py): always get complete available timeframe

--- backend-python/media_impact_monitor/data_loaders/web/google_trends.py
+++ backend-python/media_impact_monitor/data_loaders/web/google_trends.py
@@ -9,20 +9,24 @@ For the last 90 days, data is also available with daily resolution; otherwise on
 
 from time import sleep
 
-from media_impact_monitor.util.cache import cache
+import pandas as pd
 from pytrends.request import TrendReq
+
+from media_impact_monitor.util.cache import cache
 
 
 @cache
-def get_google_trends_counts(query: str):
+def get_google_trends_counts(query: str) -> pd.DataFrame:
     PyTrends = TrendReq(hl="de-DE", tz=60)
-    PyTrends.build_payload([query], timeframe="today 5-y", geo="DE")
+    PyTrends.build_payload([query], timeframe="all", geo="DE")
     df = PyTrends.interest_over_time()
     df = (
         df[~df["isPartial"]]
         .drop(columns=["isPartial"])
         .rename(columns={query: "count"})
     )
+    df.index = pd.to_datetime(df.index).date
+    df.index.name = "date"
     # when rate limit is reached, this should be 60 seconds according to https://github.com/GeneralMills/pytrends
     sleep(1)
     return df

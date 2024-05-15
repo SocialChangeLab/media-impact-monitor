0c9840ba8aa394d7089b7707402c4be852031a4f
Lucas Vogel <contact@vogelino.com>
2024-05-15 16:37:22 -0400

    feat: Show orgs of category "other" in tooltip

--- frontend-nextjs/src/components/EventsTimeline/EventsTimelineLegend.tsx
+++ frontend-nextjs/src/components/EventsTimeline/EventsTimelineLegend.tsx
@@ -4,11 +4,12 @@ import { slugifyCssClass } from "@/utility/cssSlugify";
 import type { OrganisationType } from "@/utility/eventsUtil";
 import useEvents from "@/utility/useEvents";
 import { useMemo } from "react";
+import { Portal, Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 
 function EventsTimelineLegend() {
 	const legendOrganisations = useEvents().data?.organisations;
 
-	const orgs = useMemo(() => {
+	const { allOrgs, otherOrgs } = useMemo(() => {
 		const mainOrgs: OrganisationType[] = [];
 		const otherOrgs: OrganisationType[] = [];
 
@@ -17,45 +18,80 @@ function EventsTimelineLegend() {
 			else mainOrgs.push(org);
 		}
 
-		return [
-			...mainOrgs,
-			{
-				name: "Other",
-				count: otherOrgs.reduce((acc, org) => acc + org.count, 0),
-				color: otherOrgs[0].color,
-				isMain: false,
-			},
-		];
+		return {
+			allOrgs: [
+				...mainOrgs,
+				{
+					name: "Other",
+					count: otherOrgs.reduce((acc, org) => acc + org.count, 0),
+					color: otherOrgs[0].color,
+					isMain: false,
+					orgs: otherOrgs.sort((a, b) => {
+						if (a.count > b.count) return -1;
+						if (a.count < b.count) return 1;
+						return a.name.localeCompare(b.name);
+					}),
+				},
+			],
+			otherOrgs,
+			mainOrgs,
+		};
 	}, [legendOrganisations]);
 
-	if (orgs.length === 0) return null;
+	if (allOrgs.length === 0) return null;
 	return (
 		<div className="mt-6 flex flex-col gap-2">
-			<h4 className="text-lg font-bold font-headlines antialiased relative">
+			<h4 className="text-lg font-bold font-headlines antialiased relative z-10">
 				<span className="w-fit pr-4 bg-bg relative z-20">Legend</span>
 				<span className="h-px w-full bg-grayLight absolute top-1/2 z-10"></span>
 			</h4>
-			<ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-8">
-				{orgs.map((org) => (
-					<li
-						key={org.name}
-						className={cn(
-							"grid grid-cols-[auto_1fr_auto] gap-x-2",
-							"items-center border-b border-b-grayLight py-2",
-							`legend-org legend-org-${slugifyCssClass(org.name)}`,
-							`cursor-pointer`,
-						)}
-					>
-						<span
-							className={cn(
-								"size-4 rounded-full shadow-[inset_0_0_0_1px_rgba(0,0,0,0.1)] bg-grayDark",
-							)}
-							style={{ backgroundColor: org.color }}
-							aria-hidden="true"
-						/>
-						<span className="truncate">{org.name}</span>
-						<span className="font-mono text-xs">{org.count}</span>
-					</li>
+			<ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-8 relative z-20">
+				{allOrgs.map((org) => (
+					<Tooltip key={org.name} disableHoverableContent={org.isMain}>
+						<TooltipTrigger asChild>
+							<li
+								className={cn(
+									"grid grid-cols-[auto_1fr_auto] gap-x-2",
+									"items-center border-b border-b-grayLight py-2",
+									`legend-org legend-org-${slugifyCssClass(org.name)}`,
+									`cursor-pointer`,
+								)}
+							>
+								<span
+									className={cn(
+										"size-4 rounded-full shadow-[inset_0_0_0_1px_rgba(0,0,0,0.1)] bg-grayDark",
+									)}
+									style={{ backgroundColor: org.color }}
+									aria-hidden="true"
+								/>
+								<span className="truncate">
+									{org.name}
+									{!org.isMain && ` (${org.count.toLocaleString("en-GB")})`}
+								</span>
+								<span className="font-mono text-xs">
+									{org.count.toLocaleString("en-GB")}
+								</span>
+							</li>
+						</TooltipTrigger>
+						<Portal>
+							<TooltipContent className="text-sm">
+								{!org.isMain && (
+									<ul className="flex flex-col w-96 max-w-full">
+										{otherOrgs?.map((subOrg) => (
+											<li key={subOrg.name} className="flex flex-col">
+												<div className="grid grid-cols-[1fr_auto] gap-4 py-2 border-b border-grayLight">
+													<span className="truncate">{subOrg.name}</span>
+													<span className="font-mono text-xs text-grayDark">
+														{subOrg.count.toLocaleString("en-GB")}
+													</span>
+												</div>
+											</li>
+										))}
+									</ul>
+								)}
+							</TooltipContent>
+						</Portal>
+					</Tooltip>
 				))}
 			</ul>
 		</div>
--- frontend-nextjs/src/components/ui/tooltip.tsx
+++ frontend-nextjs/src/components/ui/tooltip.tsx
@@ -10,6 +10,8 @@ const Tooltip = TooltipPrimitive.Root;
 
 const TooltipTrigger = TooltipPrimitive.Trigger;
 
+const Portal = TooltipPrimitive.Portal;
+
 const TooltipContent = React.forwardRef<
 	React.ElementRef<typeof TooltipPrimitive.Content>,
 	React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
@@ -18,12 +20,14 @@ const TooltipContent = React.forwardRef<
 		ref={ref}
 		sideOffset={sideOffset}
 		className={cn(
+			"border border-grayMed shadow-md shadow-black/10",
 			"z-50 overflow-hidden bg-pattern-semi-inverted-light px-3 py-1.5",
 			"text-xs text-brandGreen animate-in fade-in-0 zoom-in-95",
 			"data-[state=closed]:animate-out data-[state=closed]:fade-out-0",
 			"data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2",
 			"data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2",
 			"data-[side=top]:slide-in-from-bottom-2",
+			"max-h-96 overflow-y-auto",
 			className,
 		)}
 		{...props}
@@ -31,4 +35,4 @@ const TooltipContent = React.forwardRef<
 ));
 TooltipContent.displayName = TooltipPrimitive.Content.displayName;
 
-export { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger };
+export { Portal, Tooltip, TooltipContent, TooltipProvider, TooltipTrigger };

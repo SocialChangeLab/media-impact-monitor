f13bb544d3cc3a85f094800a90f0170722571569
Lucas Vogel <contact@vogelino.com>
2024-05-15 19:44:35 -0400

    refactor: Remove prefetchQuery

--- frontend-nextjs/src/app/(events)/page.tsx
+++ frontend-nextjs/src/app/(events)/page.tsx
@@ -1,23 +1,3 @@
-import { getEventsData } from "@/utility/eventsUtil";
-import { parseSearchParams } from "@/utility/searchParamsUtil";
-import { QueryClient } from "@tanstack/react-query";
-import { format } from "date-fns";
-
-export default function EventsPage({
-	searchParams,
-}: {
-	searchParams: Record<string, string>;
-}) {
-	const parsedSearchParams = parseSearchParams(
-		new URLSearchParams(searchParams),
-	);
-	const queryClient = new QueryClient();
-	const { from, to } = parsedSearchParams;
-	const formattedFrom = from && format(from, "yyyy-MM-dd");
-	const formattedTo = to && format(to, "yyyy-MM-dd");
-	queryClient.prefetchQuery({
-		queryKey: ["events", formattedFrom, formattedTo],
-		queryFn: () => getEventsData(parsedSearchParams),
-	});
+export default function EventsPage() {
 	return null;
 }

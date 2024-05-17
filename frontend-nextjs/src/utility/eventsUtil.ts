6ca05271ce119d9a375b452806f96968b88e75fe
Lucas Vogel <contact@vogelino.com>
2024-05-16 23:38:54 -0400

    feat: Fallback to local file when server fails

--- frontend-nextjs/src/utility/eventsUtil.ts
+++ frontend-nextjs/src/utility/eventsUtil.ts
@@ -75,33 +75,36 @@ export async function getEventsData(params?: {
 	if (!apiUrl)
 		throw new Error("NEXT_PUBLIC_API_URL env variable is not defined");
 	let json: unknown = { query: {}, data: [] };
-	const response = await fetch(`${apiUrl}/events`, {
-		cache: "no-store",
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-			"Cache-Control": "no-cache",
-		},
-		body: JSON.stringify({
-			source: "acled",
-			topic: "climate_change",
-			...(params?.from && params?.to
-				? {
-						start_date: format(params?.from, "yyyy-MM-dd"),
-						end_date: format(params?.to, "yyyy-MM-dd"),
-					}
-				: {}),
-		}),
-	});
+	try {
+		const response = await fetch(`${apiUrl}/events`, {
+			cache: "no-store",
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"Cache-Control": "no-cache",
+			},
+			body: JSON.stringify({
+				source: "acled",
+				topic: "climate_change",
+				...(params?.from && params?.to
+					? {
+							start_date: format(params?.from, "yyyy-MM-dd"),
+							end_date: format(params?.to, "yyyy-MM-dd"),
+						}
+					: {}),
+			}),
+		});
 
-	if (!response.ok) {
-		const message = `An error has occured: ${response.statusText} ${response.status}`;
-		throw new Error(`ApiFetchError&&&${message}`);
+		if (!response.ok) {
+			const message = `An error has occured: ${response.statusText} ${response.status}`;
+			throw new Error(`ApiFetchError&&&${message}`);
+		}
+
+		json = await response.json();
+	} catch (error) {
+		const jsonFallbackFile = Bun.file(`src/data/fallbackProtests.json`);
+		json = await jsonFallbackFile.json();
 	}
-
-	json = await response.json();
-	// json = (await import("../fakeDataForTesting/fakeEventsDataForTesting.json"))
-	// 	.default;
 	const events = validateGetDataResponse(json);
 	return events;
 }

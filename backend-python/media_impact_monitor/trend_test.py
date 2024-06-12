ea5fa99349026f406be503c04bc41b21ffeab05a
David Pomerenke <46022183+davidpomerenke@users.noreply.github.com>
2024-06-12 19:49:23 +0000

    refactor: unify issue_keywords.yaml and trend_keywords.yaml

--- backend-python/media_impact_monitor/trend_test.py
+++ backend-python/media_impact_monitor/trend_test.py
@@ -7,7 +7,7 @@ from media_impact_monitor.types_ import TrendSearch
 
 
 @pytest.mark.skip(reason="google trends currently blocked on ci")
-def test_get_trend_keywords():
+def test_get_issue_keywords():
     trend_search = TrendSearch(
         trend_type="keywords",
         media_source="web_google",

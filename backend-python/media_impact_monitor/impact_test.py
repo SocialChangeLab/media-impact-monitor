d3e2566914682d97b924d5bfc3529d72a004607f
David Pomerenke <46022183+davidpomerenke@users.noreply.github.com>
2024-06-26 17:22:21 +0000

    test: add impact tests from errors

--- backend-python/media_impact_monitor/impact_test.py
+++ backend-python/media_impact_monitor/impact_test.py
@@ -1,8 +1,6 @@
-from datetime import date
-
-import pandas as pd
 import pytest
 
+from media_impact_monitor.api import _get_impact
 from media_impact_monitor.events import get_events
 from media_impact_monitor.impact import get_impact
 from media_impact_monitor.types_ import EventSearch, Impact, ImpactSearch, TrendSearch
@@ -31,3 +29,61 @@ def test_get_impact():
     assert hasattr(result, "individual_impacts")
     assert isinstance(result.individual_impacts, dict)
     assert isinstance(result.time_series, dict)
+
+
+def test_xr():
+    get_impact(
+        ImpactSearch(
+            end_date="2024-06-25",
+            impacted_trend=TrendSearch(
+                end_date="2024-06-25",
+                media_source="news_online",
+                organizers=["Extinction Rebellion"],
+                start_date="2020-01-01",
+                topic="climate_change",
+                trend_type="sentiment",
+            ),
+            method="time_series_regression",
+            organizer="Extinction Rebellion",
+            start_date="2020-01-01",
+        )
+    )
+
+
+def test_xr_api():
+    response = _get_impact(
+        ImpactSearch(
+            end_date="2024-06-25",
+            impacted_trend=TrendSearch(
+                end_date="2024-06-25",
+                media_source="news_online",
+                organizers=["Extinction Rebellion"],
+                start_date="2020-01-01",
+                topic="climate_change",
+                trend_type="sentiment",
+            ),
+            method="time_series_regression",
+            organizer="Extinction Rebellion",
+            start_date="2020-01-01",
+        )
+    )
+
+
+@pytest.mark.skip()
+def test_fff_api():
+    response = _get_impact(
+        ImpactSearch(
+            end_date="2023-02-19",
+            impacted_trend=TrendSearch(
+                end_date="2023-02-19",
+                media_source="news_online",
+                organizers=["Fridays for Future"],
+                start_date="2022-09-14",
+                topic="climate_change",
+                trend_type="sentiment",  # sentiment is only available from 2024
+            ),
+            method="time_series_regression",
+            organizer="Fridays for Future",
+            start_date="2022-09-14",
+        )
+    )

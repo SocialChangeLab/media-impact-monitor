327a4465101b638ba9e7385b4f1d8e36524e4e5d
Lucas Vogel <contact@vogelino.com>
2024-06-28 00:15:26 -0400

    fix(EventsTimelineSizeLegend): Fix same height legend items

--- frontend-nextjs/src/components/EventsTimeline/EventsTimelineSizeLegend.tsx
+++ frontend-nextjs/src/components/EventsTimeline/EventsTimelineSizeLegend.tsx
@@ -13,8 +13,9 @@ function EventsTimelineSizeLegend({
 }) {
 	const exampleSizes = useMemo(() => {
 		const max = roundLegendNumber(Math.max(sizeScale.domain()[1], 30));
-		const min = getLegendMinNumber(max);
-		const mid = roundLegendNumber(Math.floor((min + max) / 2));
+		let min = getLegendMinNumber(max);
+		min = min === max ? max / 2 : min;
+		const mid = Math.floor((min + max) / 2);
 		return [undefined, min, mid, max].map((x, idx) => ({
 			id: idx,
 			size: x,

d33d9dbf4316fbd0c62cef4280d81db501953415
Lucas Vogel <contact@vogelino.com>
2024-06-27 22:40:43 -0400

    refactor(UptimeStatusLink): Simplify (no data fetching, hardcoded monitors)

--- frontend-nextjs/src/components/UptimeStatusLink.tsx
+++ frontend-nextjs/src/components/UptimeStatusLink.tsx
@@ -2,50 +2,23 @@
 import { cn } from "@/utility/classNames";
 import { useQuery } from "@tanstack/react-query";
 import { ExternalLink } from "lucide-react";
-import { Fragment, forwardRef, useMemo } from "react";
-import { z } from "zod";
+import { forwardRef } from "react";
 import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 
 const UPTIME_KUMA_BASE_URL =
 	process.env.NEXT_PUBLIC_UPTIME_KUMA_BASE_URL ||
 	`https://status.mediaimpactmonitor.app`;
 
-const ConfigSchema = z.object({
-	slug: z.string(),
-	title: z.string(),
-	description: z.string(),
-	icon: z.string(),
-	theme: z.enum(["auto", "light", "dark"]).optional(), // `theme` might have specific values
-	published: z.boolean(),
-	showTags: z.boolean(),
-	customCSS: z.string().nullable(),
-	footerText: z.string().nullable(),
-	showPoweredBy: z.boolean(),
-	googleAnalyticsId: z.string().nullable(),
-	showCertificateExpiry: z.boolean(),
-});
-
-const MonitorSchema = z.object({
-	id: z.number(),
-	name: z.string(),
-});
-type MonitorType = z.infer<typeof MonitorSchema>;
-
-const PublicGroupSchema = z.object({
-	id: z.number(),
-	name: z.string(),
-	weight: z.number(),
-	monitorList: z.array(MonitorSchema),
-});
-
-const UptimeStatusSchema = z.object({
-	config: ConfigSchema,
-	incident: z.null(),
-	publicGroupList: z.array(PublicGroupSchema),
-	maintenanceList: z.array(z.unknown()), // Assuming we don't have details about the structure of maintenanceList items
-});
+type MonitorType = {
+	id: number;
+	name: string;
+};
 
 type StatusType = "up" | "down" | "unknown" | "partial";
+
+type MonitorWithStatusType = MonitorType & {
+	status: StatusType;
+};
 
 const UptimeStatusLinkAnchor = forwardRef<
 	HTMLAnchorElement,
@@ -80,65 +53,28 @@ function UptimeStatusPill({ status }: { status: StatusType }) {
 	);
 }
 
+// INFO: Gather new monitors from https://metamonitor.up.railway.app/api/status-page/main
 const monitors: MonitorType[] = [
-	{
-		id: 1,
-		name: "production website",
-	},
-	{
-		id: 3,
-		name: "production api docs",
-	},
-	{
-		id: 2,
-		name: "staging website",
-	},
-	{
-		id: 4,
-		name: "staging api docs",
-	},
-	{
-		id: 5,
-		name: "staging api events",
-	},
-	{
-		id: 6,
-		name: "staging api trend news_online keywords",
-	},
-	{
-		id: 8,
-		name: "staging api trend news_online sentiment",
-	},
-	{
-		id: 7,
-		name: "staging api trend news_print keywords",
-	},
-	{
-		id: 9,
-		name: "staging api trend web_google keywords",
-	},
-	{
-		id: 10,
-		name: "staging api fulltexts",
-	},
-	{
-		id: 11,
-		name: "staging api impact",
-	},
+	{ id: 1, name: "production website" },
+	{ id: 3, name: "production api docs" },
+	{ id: 2, name: "staging website" },
+	{ id: 4, name: "staging api docs" },
+	{ id: 5, name: "staging api events" },
+	{ id: 6, name: "staging api trend news_online keywords" },
+	{ id: 8, name: "staging api trend news_online sentiment" },
+	{ id: 7, name: "staging api trend news_print keywords" },
+	{ id: 9, name: "staging api trend web_google keywords" },
+	{ id: 10, name: "staging api fulltexts" },
+	{ id: 11, name: "staging api impact" },
 ];
 
 function getBadgeUrl(monitorId: number) {
-	const config = {
-		style: "flat-square",
-		prefixLabel: "Impact",
-		upColor: "#229E74",
-		downColor: "#D55E00",
-	};
+	const config = { style: "flat-square" };
 	const query = new URLSearchParams(config).toString();
 	return `${UPTIME_KUMA_BASE_URL}/api/badge/${monitorId}/status?${query}`;
 }
 
-async function fetchMonitorUrl(monitor: MonitorType) {
+async function fetchMonitor(monitor: MonitorType) {
 	try {
 		const response = await fetch(getBadgeUrl(monitor.id));
 		const svgText = await response.text();
@@ -161,24 +97,24 @@ async function fetchMonitorUrl(monitor: MonitorType) {
 	}
 }
 
+function getOverallStatus(monitors: MonitorWithStatusType[]): StatusType {
+	if (!monitors) return "unknown";
+	const allUp = monitors.every((monitor) => monitor.status === "up");
+	const allDown = monitors.every((monitor) => monitor.status === "down");
+	if (allUp) return "up";
+	if (allDown) return "down";
+	return "partial";
+}
+
 async function fetchMonitors() {
-	let monitors: MonitorType[] = fallbackMonitors;
-	try {
-		const response = await fetch(
-			`${UPTIME_KUMA_BASE_URL}/api/status-page/main`,
-		);
-		const json = await response.json();
-		const parsed = UptimeStatusSchema.parse(json);
-		monitors =
-			parsed.publicGroupList.find(({ name }) => name === "Services")
-				?.monitorList ?? fallbackMonitors;
-	} catch (error) {
-		console.error(error);
-	}
-	const imageObjects = await Promise.all(
-		monitors.map((monitor) => fetchMonitorUrl(monitor)),
+	const monitorObjects = await Promise.all(
+		monitors.map((monitor) => fetchMonitor(monitor)),
 	);
-	return imageObjects;
+	const overallStatus = getOverallStatus(monitorObjects);
+	return {
+		monitors: monitorObjects,
+		overallStatus: overallStatus,
+	};
 }
 
 function UptimeStatusLink() {
@@ -187,38 +123,21 @@ function UptimeStatusLink() {
 		queryFn: fetchMonitors,
 	});
 
-	const overallStatus = useMemo((): StatusType => {
-		if (!query.data) return "unknown";
-		const allUp = query.data.every((monitor) => monitor.status === "up");
-		const allDown = query.data.every((monitor) => monitor.status === "down");
-		if (allUp) return "up";
-		if (allDown) return "down";
-		return "partial";
-	}, [query.data]);
-
 	if (!query.isSuccess || !query.data)
-		return <UptimeStatusLinkAnchor status={overallStatus} />;
+		return <UptimeStatusLinkAnchor status="unknown" />;
 
 	return (
 		<Tooltip delayDuration={50} disableHoverableContent>
 			<TooltipTrigger className="inline-block w-fit bg-none">
-				<UptimeStatusLinkAnchor status={overallStatus} />
+				<UptimeStatusLinkAnchor status={query.data.overallStatus} />
 			</TooltipTrigger>
 			<TooltipContent className="w-fit" align="start">
-				<ul
-					className="inline-grid grid-cols-[auto_auto] gap-x-2 gap-y-1 items-center"
-					style={{
-						gridTemplateRows: `repeat(${query.data.length}, 20px)`,
-					}}
-				>
-					{query.data.map((monitor) => (
-						<Fragment key={monitor.id}>
+				<ul className="flex flex-col gap-1">
+					{query.data.monitors.map((monitor) => (
+						<li key={monitor.id} className="flex gap-2">
 							<UptimeStatusPill status={monitor.status} />
-							{toTitleCase(monitor.name.replace("_", " ")).replace(
-								" Api ",
-								" API ",
-							)}
-						</Fragment>
+							{formatMonitorName(monitor.name)}
+						</li>
 					))}
 				</ul>
 			</TooltipContent>
@@ -227,9 +146,11 @@ function UptimeStatusLink() {
 }
 export default UptimeStatusLink;
 
-function toTitleCase(str: string) {
-	return str.replace(
+function formatMonitorName(str: string) {
+	const nameWithoutUnderscores = str.replace("_", " ");
+	const nameInTitleCase = nameWithoutUnderscores.replace(
 		/\w\S*/g,
 		(txt) => txt.charAt(0).toUpperCase() + txt.substr(1),
 	);
+	return nameInTitleCase.replace(" Api ", " API ");
 }

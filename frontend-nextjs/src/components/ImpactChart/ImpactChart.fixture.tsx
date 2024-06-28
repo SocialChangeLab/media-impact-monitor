b902cbe4bea16032179c6677665fe29b983014ac
Lucas Vogel <contact@vogelino.com>
2024-06-27 23:04:55 -0400

    fix(ImpactChart): Fix cosmos fixtures

--- frontend-nextjs/src/components/ImpactChart/ImpactChart.fixture.tsx
+++ frontend-nextjs/src/components/ImpactChart/ImpactChart.fixture.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Providers from "@/appProviders";
 import useDebounce from "@custom-react-hooks/use-debounce";
 import { useState } from "react";
 import { Button } from "../ui/button";
@@ -60,35 +61,37 @@ function ImpactChartFixture() {
 		},
 	);
 	return (
-		<div className="w-full h-screen flex justify-center items-center p-8">
-			<div className="border border-dashed border-grayLight p-8 rounded-lg bg-pattern-soft w-full flex flex-col gap-12">
-				<div className="flex gap-6">
-					<Button onClick={updateColumns}>Randomise Bars</Button>
-					<label htmlFor="maxImpact" className="flex flex-col">
-						<span>Max impact: {maxImpact.toLocaleString()}</span>
-						<input
-							className="accent-fg w-40"
-							id="maxImpact"
-							type="range"
-							min={100}
-							max={10000}
-							step={100}
-							value={maxImpact}
-							onChange={(e) => {
-								const newMaxImpact = Number.parseInt(e.target.value, 10);
-								setMaxImpact(newMaxImpact);
-								updateColumns();
-							}}
-						/>
-					</label>
+		<Providers>
+			<div className="w-full h-screen flex justify-center items-center p-8">
+				<div className="border border-dashed border-grayLight p-8 rounded-lg bg-pattern-soft w-full flex flex-col gap-12">
+					<div className="flex gap-6">
+						<Button onClick={updateColumns}>Randomise Bars</Button>
+						<label htmlFor="maxImpact" className="flex flex-col">
+							<span>Max impact: {maxImpact.toLocaleString()}</span>
+							<input
+								className="accent-fg w-40"
+								id="maxImpact"
+								type="range"
+								min={100}
+								max={10000}
+								step={100}
+								value={maxImpact}
+								onChange={(e) => {
+									const newMaxImpact = Number.parseInt(e.target.value, 10);
+									setMaxImpact(newMaxImpact);
+									updateColumns();
+								}}
+							/>
+						</label>
+					</div>
+					<ImpactChart
+						columns={columns}
+						unitLabel="articles and media"
+						icon="LineChart"
+					/>
 				</div>
-				<ImpactChart
-					columns={columns}
-					unitLabel="articles and media"
-					icon="LineChart"
-				/>
 			</div>
-		</div>
+		</Providers>
 	);
 }
 

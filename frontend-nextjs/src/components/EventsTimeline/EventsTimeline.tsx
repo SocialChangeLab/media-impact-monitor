3e201230973a918e45ddfd99f72804630182273e
Lucas Vogel <contact@vogelino.com>
2024-07-02 15:47:42 -0400

    feat(EventsTimeline): Add dataset instertion indicator to the protests timeline

--- frontend-nextjs/src/components/EventsTimeline/EventsTimeline.tsx
+++ frontend-nextjs/src/components/EventsTimeline/EventsTimeline.tsx
@@ -1,11 +1,17 @@
 "use client";
 import LoadingEventsTimeline from "@/components/EventsTimeline/LoadingEventsTimeline";
 import { cn } from "@/utility/classNames";
+import {
+	dateToComparableDateItem,
+	type ComparableDateItemType,
+} from "@/utility/comparableDateItemSchema";
 import { parseErrorMessage } from "@/utility/errorHandlingUtil";
 import type { OrganisationType, ParsedEventType } from "@/utility/eventsUtil";
 import useEvents from "@/utility/useEvents";
+import { isInSameAggregationUnit } from "@/utility/useTimeIntervals";
 import useElementSize from "@custom-react-hooks/use-element-size";
 import { QueryErrorResetBoundary } from "@tanstack/react-query";
+import { format } from "date-fns";
 import { ErrorBoundary } from "next/dist/client/components/error-boundary";
 import { Suspense } from "react";
 import CollapsableSection from "../CollapsableSection";
@@ -22,6 +28,26 @@ import EventsTimelineScrollWrapper from "./EventsTimelineScrollWrapper";
 import EventsTimelineSizeLegend from "./EventsTimelineSizeLegend";
 import useAggregationUnit from "./useAggregationUnit";
 import useTimelineEvents from "./useTimelineEvents";
+
+type DataSourceInsertionType = {
+	date: ComparableDateItemType;
+	name: string;
+};
+
+const dataSourceInsertions: DataSourceInsertionType[] = [
+	{
+		date: dateToComparableDateItem(new Date("2023-05-01T00:00:00.000Z")),
+		name: "ACLED (Armed Conflict Location & Event Data Project)",
+	},
+	{
+		date: dateToComparableDateItem(new Date("2024-05-10T00:00:00.000Z")),
+		name: "Press Releases by Last Generation (Germany)",
+	},
+	{
+		date: dateToComparableDateItem(new Date("2020-05-20T00:00:00.000Z")),
+		name: "GENIOS (German News Online)",
+	},
+];
 
 function EventsTimeline({
 	data,
@@ -46,41 +72,75 @@ function EventsTimeline({
 			<EventsTimelineScrollWrapper>
 				<EventsTimelineChartWrapper columnsCount={columnsCount + 1}>
 					{eventColumns.map(
-						({ time, date, eventsWithSize, sumSize, combinedOrganizers }) => (
-							<li
-								key={`event-day-${time}`}
-								className="grid grid-rows-subgrid row-span-3 relative w-4 grow shrink-0 h-full py-4"
-							>
-								<div className="flex flex-col justify-end items-center gap-0.5">
-									<div
-										className={cn(
-											"w-px h-full absolute top-0 left-1/2 -translate-x-1/2",
-											"bg-grayLight group-hover:opacity-50 event-line opacity-0 transition-opacity",
-										)}
-										aria-hidden="true"
-									/>
-									{aggregationUnit === "day" &&
-										eventsWithSize.map((event) => (
-											<EventTimelineItem
-												key={event.event_id}
-												event={event}
-												organisations={organisations}
-												height={sizeScale(event.size_number ?? 0)}
+						({ time, date, eventsWithSize, sumSize, combinedOrganizers }) => {
+							const insertedDataSource = dataSourceInsertions.find((d) =>
+								isInSameAggregationUnit(aggregationUnit, d.date, date),
+							);
+							const indexOfDataSourceInsertion = dataSourceInsertions.findIndex(
+								(d) => d === insertedDataSource,
+							);
+							const colIdx = eventColumns.findIndex((d) => d.date === date);
+							const colsAfter = eventColumns.length - colIdx - 1;
+							const dataSourceTop = `${indexOfDataSourceInsertion * 3}rem`;
+							return (
+								<li
+									key={`event-day-${time}`}
+									className="grid grid-rows-subgrid row-span-3 relative w-4 grow shrink-0 h-full py-4"
+								>
+									<div className="flex flex-col justify-end items-center gap-0.5">
+										<div
+											className={cn(
+												"w-px h-full absolute top-0 left-1/2 -translate-x-1/2 pointer-events-none",
+												"bg-grayLight group-hover:opacity-50 event-line opacity-0 transition-opacity",
+												insertedDataSource &&
+													"opacity-100 group-hover:opacity-100 bg-gradient-to-b via-grayLight from-fg to-grayUltraLight",
+											)}
+											aria-hidden={!insertedDataSource}
+										>
+											{insertedDataSource && (
+												<span
+													className={cn(
+														"grid absolute top-0 p-4 text-sm select-none truncate",
+														"opacity-0 transition-opacity group-hover:opacity-100",
+														colsAfter < 10 ? "right-px text-right" : "left-px",
+													)}
+													style={{ top: dataSourceTop }}
+												>
+													<span className="uppercase text-xs text-grayDark opacity-80">
+														{format(
+															insertedDataSource.date.date,
+															"LLLL d, yyyy",
+														)}
+														{" • "}
+														Dataset Insertion
+													</span>
+													{insertedDataSource.name}
+												</span>
+											)}
+										</div>
+										{aggregationUnit === "day" &&
+											eventsWithSize.map((event) => (
+												<EventTimelineItem
+													key={event.event_id}
+													event={event}
+													organisations={organisations}
+													height={sizeScale(event.size_number ?? 0)}
+												/>
+											))}
+										{aggregationUnit !== "day" && eventsWithSize.length > 0 && (
+											<EventsTimelineAggregatedItem
+												date={date}
+												sumSize={sumSize}
+												height={Math.ceil(sizeScale(sumSize))}
+												organisations={combinedOrganizers}
+												events={eventsWithSize}
+												aggregationUnit={aggregationUnit}
 											/>
-										))}
-									{aggregationUnit !== "day" && eventsWithSize.length > 0 && (
-										<EventsTimelineAggregatedItem
-											date={date}
-											sumSize={sumSize}
-											height={Math.ceil(sizeScale(sumSize))}
-											organisations={combinedOrganizers}
-											events={eventsWithSize}
-											aggregationUnit={aggregationUnit}
-										/>
-									)}
-								</div>
-							</li>
-						),
+										)}
+									</div>
+								</li>
+							);
+						},
 					)}
 				</EventsTimelineChartWrapper>
 				<EventsTimelineAxis

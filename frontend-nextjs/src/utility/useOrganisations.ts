dbd4fb6623a1e2f3dbb3bdca9e36889a4b3aca8b
Lucas Vogel <contact@vogelino.com>
2024-09-01 12:03:14 -0400

    fix(useEvents): Remove unused params

--- frontend-nextjs/src/utility/useOrganisations.ts
+++ frontend-nextjs/src/utility/useOrganisations.ts
@@ -1,13 +1,8 @@
-import { parse, startOfDay } from "date-fns";
 import type { EventOrganizerSlugType } from "./eventsUtil";
-import { today } from "./today";
 import useEvents from "./useEvents";
 
 export function useOrganisations() {
-	const { data, isPending } = useEvents({
-		from: startOfDay(parse("01-01-2020", "dd-MM-yyyy", today)),
-		to: today,
-	});
+	const { data, isPending } = useEvents();
 
 	return {
 		organisations: data?.organisations ?? [],

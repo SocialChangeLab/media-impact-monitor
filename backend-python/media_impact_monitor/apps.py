c78de8e1181a79112c8aee2f47286d51f28b9fca
David Pomerenke <46022183+davidpomerenke@users.noreply.github.com>
2024-12-07 16:26:44 +0100

    feat(backend): basic django init

---
+++ backend-python/media_impact_monitor/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MediaImpactMonitorDjangoConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "media_impact_monitor_django"

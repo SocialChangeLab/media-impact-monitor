c091c6007746aa462e14b29397dafa1b8b7db766
David Pomerenke <46022183+davidpomerenke@users.noreply.github.com>
2024-12-08 23:04:01 +0100

    refactor(django): copy over header (but not yet perfect)

--- backend-python/media_impact_monitor/urls.py
+++ backend-python/media_impact_monitor/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("dashboard", views.dashboard, name="dashboard"),
+    path("organisations", views.organisations, name="organisations"),
+    path("about", views.about, name="about"),
+    path("docs", views.docs, name="docs"),
     path("event/<event_id>", views.event_detail, name="event detail")
 ]
--- backend-python/media_impact_monitor/views.py
+++ backend-python/media_impact_monitor/views.py
@@ -2,9 +2,24 @@ from django.shortcuts import render, get_object_or_404
 from .models import Event
 
 def index(request):
-    events = Event.objects.all()[:20]
+    events = Event.objects.all()[:50]
     return render(request, "index.html", {"events": events})
+
+def dashboard(request):
+    return render(request, "dashboard.html")
 
 def event_detail(request, event_id):
     event = get_object_or_404(Event, event_id=event_id)
     return render(request, "event_detail.html", {"event": event})
+
+def organisations(request):
+    return render(request, "organisations.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def docs(request):
+    return render(request, "docs.html")
+
+
+                  

ab9d9f568bd1a953f81d4969f94d899d1071afbf
David Pomerenke <46022183+davidpomerenke@users.noreply.github.com>
2024-12-08 15:28:09 +0100

    refactor(django): set up dummy view templates

--- backend-python/media_impact_monitor/urls.py
+++ backend-python/media_impact_monitor/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("event/<event_id>", views.event_detail, name="event detail")
 ]
--- backend-python/media_impact_monitor/views.py
+++ backend-python/media_impact_monitor/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
-
+from django.shortcuts import render, get_object_or_404
+from .models import Event
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-    
+    events = Event.objects.all()
+    return render(request, "index.html", {"events": events})
+
+def event_detail(request, event_id):
+    event = get_object_or_404(Event, event_id=event_id)
+    return render(request, "event_detail.html", {"event": event})
